a3301e9dbf95726052eb1f63266d36ba7e1b3e5e
Rummy <cerealbh@gmail.com>
2022-03-03 17:36:17 -0600

    Added array translation

--- ato.go
+++ ato.go
@@ -39,30 +39,63 @@ func turtle(i map[interface{}]interface{}, offset int) {
 	for k, v := range i {
 		switch v.(type) {
 		case string:
-			fmt.Printf("%s%s:\n", pad(offset), k)
-			fmt.Printf("%stype: string\n", pad(padLevel(offset)))
+			String(k.(string), offset)
 		case int:
-			fmt.Printf("%s%s:\n", pad(offset), k)
-			fmt.Printf("%stype: integer\n", pad(padLevel(offset)))
+			Int(k.(string), offset)
 		case bool:
+			Bool(k.(string), offset)
+		case []interface{}:
 			fmt.Printf("%s%s:\n", pad(offset), k)
-			fmt.Printf("%stype: boolean\n", pad(padLevel(offset)))
+			fmt.Printf("%stype: array\n", pad(pad2(offset)))
+			fmt.Printf("%sitems:\n", pad(pad2(offset)))
+			for _, arr := range v.([]interface{}) {
+				array := make(map[interface{}]interface{})
+				array[""] = arr
+				turtle(array, pad2(offset))
+			}
 		case map[interface{}]interface{}:
-			fmt.Printf("%s%s:\n", pad(offset), k)
-			fmt.Printf("%stype: object\n", pad(padLevel(offset)))
-			fmt.Printf("%sproperties:\n", pad(padLevel(offset)))
-			turtle(v.(map[interface{}]interface{}), padLevel(offset+2))
+			Object(k.(string), offset)
+			turtle(v.(map[interface{}]interface{}), pad2(offset+2))
 		default:
 			fmt.Printf("unknown type: %T", v)
 		}
 	}
 }
 
+func String(name string, offset int) {
+	if name != "" {
+		fmt.Printf("%s%s:\n", pad(offset), name)
+	}
+	fmt.Printf("%stype: string\n", pad(pad2(offset)))
+}
+
+func Int(name string, offset int) {
+	if name != "" {
+		fmt.Printf("%s%s:\n", pad(offset), name)
+	}
+	fmt.Printf("%stype: integer\n", pad(pad2(offset)))
+}
+
+func Bool(name string, offset int) {
+	if name != "" {
+		fmt.Printf("%s%s:\n", pad(offset), name)
+	}
+	fmt.Printf("%stype: boolean\n", pad(pad2(offset)))
+}
+
+func Object(name string, offset int) {
+	if name != "" {
+		fmt.Printf("%s%s:\n", pad(offset), name)
+	}
+	fmt.Printf("%stype: object\n", pad(pad2(offset)))
+	fmt.Printf("%sproperties:\n", pad(pad2(offset)))
+}
+
 func pad(i int) string {
 	return strings.Repeat(" ", i)
 }
 
-func padLevel(i int) int {
+func pad2(i int) int {
 	return i + 2
 }
 
